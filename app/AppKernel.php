4df1bd07eb7f8ec446fa60cb9077b36dd1ba27ed
Mustapha <mustapha.benhajminyaoui@esprit.tn>
2019-02-11 15:12:48 +0100

    Integration de la template Backoffice

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FrontBundle\FrontBundle(),
+            new BackBundle\BackBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
