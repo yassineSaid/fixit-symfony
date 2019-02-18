996d6063ef9f3722863c77046f024a43dcf36a6f
Ali.saidani <ali.saidani@esprit.tn>
2019-02-18 10:43:26 +0100

    crud a

--- src/MainBundle/Controller/CategorieProduitController.php
+++ src/MainBundle/Controller/CategorieProduitController.php
@@ -19,9 +19,50 @@ class CategorieProduitController extends Controller
             $em->flush();
 
             return $this->redirectToRoute("main_ajouterCategori_Produit");
+
         }
 
         return $this->render("@Back/CategorieProduit/CategorieProduit.html.twig");
 
     }
+    public function ListerAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $categorie=$em->getRepository(CategorieProduit::class)->findAll();
+        return $this->render('@Back/CategorieProduit/listCategProd.html.twig',array("recs"=>$categorie));
+
+    }
+    public function modifierAction($cat,Request $request)
+    {
+        if ($this->getUser()==null)
+        {
+            return $this->redirectToRoute('login');
+        }
+        $em=$this->getDoctrine()->getManager();
+        $em=$this->getDoctrine()->getManager();
+        $categorie=$em->getRepository(CategorieProduit::class)->find($cat);
+
+        if($request->isMethod('POST'))
+        {
+
+            $categorie->setNom($request->get("categorie"));
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('main_listCategorie_Produit');
+        }
+        return $this->render('@Back/CategorieProduit/modifierCategorie.html.twig');
+    }
+    public function supprimerAction($cat)
+    {
+        if ($this->getUser()==null)
+        {
+            return $this->redirectToRoute('login');
+        }
+        $em=$this->getDoctrine()->getManager();
+        $categorie=$em->getRepository(CategorieProduit::class)->find($cat);
+        $em->remove($categorie);
+        $em->flush();
+        return $this->redirectToRoute("main_listCategorie_Produit");
+    }
+
 }
