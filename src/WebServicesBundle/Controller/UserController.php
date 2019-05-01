81baad7a6b4b23eb3a478297c9b46dea8f679daa
yassine said <yassine.said@esprit.tn>
2019-05-01 11:22:41 +0100

    Ajout de modification du profil WEB Service

--- src/WebServicesBundle/Controller/UserController.php
+++ src/WebServicesBundle/Controller/UserController.php
@@ -19,4 +19,26 @@ class UserController extends Controller
         $formatted = $serializer->normalize($user);
         return new JsonResponse($formatted);
     }
+    public function getUserByIdAction(Request $request)
+    {
+        $connect=$this->getDoctrine()->getManager();
+        $user=$connect->getRepository(User::class)->find($request->get("id"));
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($user);
+        return new JsonResponse($formatted);
+    }
+    public function modifierUserAction(Request $request)
+    {
+        if ($request->isMethod("post")) {
+            $connect = $this->getDoctrine()->getManager();
+            $user = $connect->getRepository(User::class)->find($request->get("id"));
+            $user->setFirstname($request->get("firstname"));
+            $user->setLastname($request->get("lastname"));
+            $user->setAddress($request->get("address"));
+            $connect->persist($user);
+            $connect->flush();
+            return new JsonResponse(200);
+        }
+        return new JsonResponse(500);
+    }
 }
